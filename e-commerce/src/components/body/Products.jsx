99c7468b8b077eeb8b0582990eecce11b5cf64b7
Oluwaseyi-Java <adeseyitu@gmail.com>
2022-05-25 08:38:13 +0100

    Adjustment

--- e-commerce/src/components/body/Products.jsx
+++ e-commerce/src/components/body/Products.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import HeadsetImg from "../../Images/Ecommerce-stock-image/toppng.com-headphone-669x846.png"
+// import HeadsetImg from "../../Images/Ecommerce-stock-image/toppng.com-headphone-669x846.png"
 
-import cartImg from "../../Images/Ecommerce-stock-image/Cart-Transparent-PNG.png"
+// import cartImg from "../../Images/Ecommerce-stock-image/Cart-Transparent-PNG.png"
+
+// <img src={HeadsetImg} alt="pic" />
 
 
-
+// <img src={cartImg} alt="pic" />
 const Products = () => {
     return (
         <div className='ProductComponentContainer'>
-            <img src={HeadsetImg} alt="pic" />
             <h2>Nemo enim ipsam</h2>
             <p>$180</p>
-            <img src={cartImg} alt="pic" />
         </div>
     )
 }
